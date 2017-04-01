0757476b8e3683a44a708c43cff63b1ba8e8646f
Daniel Bastos <daniel.bastos1710@gmail.com>
2017-04-01 00:02:18 -0300

    Update Led.ino

--- 1 - Led/Led.ino
+++ 1 - Led/Led.ino
@@ -3,8 +3,8 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
+  	digitalWrite(2, HIGH);
+  	delay(1000);
+  	digitalWrite(2, LOW);
+  	delay(1000);
 }
